d477794653a59fac5e1f5495aaac27eb26934de7
Denis Lantsman <dlantsman@amplify.com>
2024-12-30 11:49:13 -0800

    improve findReferences and hover tools

---
+++ bun/tools/hover.spec.ts
@@ -0,0 +1,66 @@
+import { type ToolRequestId } from "./toolManager.ts";
+import { describe, it, expect } from "bun:test";
+import { withDriver } from "../test/preamble";
+import { pollUntil } from "../utils/async.ts";
+
+describe("bun/tools/hover.spec.ts", () => {
+  it("hover end-to-end", async () => {
+    await withDriver(async (driver) => {
+      await driver.editFile("bun/test/fixtures/test.ts");
+      await driver.showSidebar();
+
+      await driver.inputMagentaText(`Try hovering a symbol`);
+      await driver.send();
+
+      const toolRequestId = "id" as ToolRequestId;
+      await driver.mockAnthropic.respond({
+        stopReason: "tool_use",
+        text: "ok, here goes",
+        toolRequests: [
+          {
+            status: "ok",
+            value: {
+              type: "tool_use",
+              id: toolRequestId,
+              name: "hover",
+              input: {
+                filePath: "bun/test/fixtures/test.ts",
+                symbol: "val.a.b.c",
+              },
+            },
+          },
+        ],
+      });
+
+      const result = await pollUntil(
+        () => {
+          const state = driver.magenta.chatApp.getState();
+          if (state.status != "running") {
+            throw new Error(`app crashed`);
+          }
+
+          const toolWrapper =
+            state.model.toolManager.toolWrappers[toolRequestId];
+          if (!toolWrapper) {
+            throw new Error(
+              `could not find toolWrapper with id ${toolRequestId}`,
+            );
+          }
+
+          if (toolWrapper.model.state.state != "done") {
+            throw new Error(`Request not done`);
+          }
+
+          return toolWrapper.model.state.result;
+        },
+        { timeout: 3000 },
+      );
+
+      expect(result).toEqual({
+        tool_use_id: toolRequestId,
+        type: "tool_result",
+        content: `(markdown):\n\n\`\`\`typescript\n(property) c: \"test\"\n\`\`\`\n\n`,
+      });
+    });
+  });
+});
--- minimal-init.lua
+++ minimal-init.lua
@@ -1,2 +1,19 @@
 vim.opt.runtimepath:append(".")
 require("magenta")
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    -- This handler will fire when the buffer's 'filetype' is "python"
+    pattern = "typescript",
+    callback = function(ev)
+      vim.lsp.start(
+        {
+          name = "ts_ls",
+          cmd = {"typescript-language-server", "--stdio"},
+          root_dir = vim.fs.root(ev.buf, {"tsconfig.json", "package.json"})
+        }
+      )
+    end
+  }
+)
